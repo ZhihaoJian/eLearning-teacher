c2c4b07c0c2b810e0689c23818236502ed2a6183
ZhihaoJian <jianzhihao123@gmail.com>
2018-05-17 09:55:49 +0800

    tree组件组织优化

---
+++ src/components/Tree/NewTree.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Collapse, Tree } from 'antd';
+import './index.scss';
+import FaFolder from 'react-icons/lib/fa/folder-o';
+import FaPlus from 'react-icons/lib/fa/plus';
+
+const ADD_ROOT_FILE = 'ADD_ROOT_FILE';
+const ADD_ROOT_FOLDER = 'ADD_ROOT_FOLDER';
+
+export default class NewTree extends React.Component {
+
+    render() {
+        return (
+            <div className='tree' >
+                <Collapse
+                    defaultActiveKey={'1'}
+                >
+                    <Collapse.Panel
+                        key={'1'}
+                        header={
+                            (
+                                <div className='add-node' onClick={this.props.addRootNode} >
+                                    <a data-type={ADD_ROOT_FILE} title='新建文本文件'><FaPlus /></a>
+                                    <a data-type={ADD_ROOT_FOLDER} title='新建文件夹'><FaFolder /></a>
+                                </div>
+                            )
+                        }
+                    >
+                        <Tree
+                            draggable
+                            showIcon
+                            defaultSelectedKeys={this.props.selectedKeys}
+                            onSelect={this.props.onSelect}
+                            onRightClick={this.props.onRightClick}
+                            onDrop={this.props.onDrop}
+                        >
+                            {this.props.loop(this.props.gData)}
+                        </Tree>
+                    </Collapse.Panel>
+                </Collapse>
+            </div>
+        );
+    }
+}
