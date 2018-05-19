8d62beb9b84bc7f3a325ddced2e4210b0949bd49
ZhihaoJian <jianzhihao123@gmail.com>
2018-05-19 09:56:12 +0800

    添加redux-persist做状态持久化处理

--- src/redux/app.redux.js
+++ src/redux/app.redux.js
@@ -1,18 +1,21 @@
-import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
 import thunkMiddleware from 'redux-thunk';
-import { examsReducers } from './AddExam.redux';
-import { addNoteReducers } from './AddNote.redux';
+import rootReducer from './reducers.redux';
+import storage from 'redux-persist/lib/storage';
 
-
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['addNoteReducers', 'examsReducers']
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 const reduxDevTools = window.devToolsExtension ? window.devToolsExtension() : () => { };
 
 export const store = createStore(
-    combineReducers({
-        examsReducers,
-        addNoteReducers
-    }),
-    compose(applyMiddleware(thunkMiddleware))
+    persistedReducer,
+    compose(applyMiddleware(thunkMiddleware), reduxDevTools)
 );
 
-
+export const persistor = persistStore(store);
 
---
+++ src/redux/reducers.redux.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import { examsReducers } from './AddExam.redux';
+
+
+export default combineReducers({
+    examsReducers
+})
+
