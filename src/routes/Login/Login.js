73748c3ed794b0b8d23c9df36afd90151bea3d6e
ZhihaoJian <jianzhihao123@gmail.com>
2018-07-02 20:50:20 +0800

    新增登录注册功能

--- src/routes/Login/Login.js
+++ src/routes/Login/Login.js
@@ -1,47 +1,56 @@
 import React from 'react';
 import Login from 'ant-design-pro/lib/Login';
-import { Alert, Checkbox } from 'antd';
+import { Alert, Checkbox, Icon } from 'antd';
 import Footer from '../../components/GlobalFooter/GlobalFooter';
 import './index.scss';
+import { UserLogin } from '../../service/User.service';
 const { Tab, UserName, Password, Mobile, Captcha, Submit } = Login;
 
 class LoginPage extends React.Component {
     state = {
         notice: '',
-        type: 'tab2',
+        type: 'tab1',
         autoLogin: true,
     }
+
     onSubmit = (err, values) => {
-        console.log('value collected ->', { ...values, autoLogin: this.state.autoLogin });
-        if (this.state.type === 'tab1') {
+        const { type } = this.state;
+        if (type === 'tab1') {
             this.setState({
                 notice: '',
             }, () => {
-                if (!err && (values.username !== 'admin' || values.password !== '888888')) {
-                    setTimeout(() => {
-                        this.setState({
-                            notice: 'The combination of username and password is incorrect!',
-                        });
-                    }, 500);
+                if (!err && values.username && values.password) {
+                    UserLogin(values).then(res => {
+                        if (res) {
+                            window.location.href = '/classroom';
+                        }
+                    })
+                } else {
+                    this.setState({ notice: '请检查账号与密码是否正确' })
                 }
             });
+        } else {
+            //TODO 
+            //手机登录
         }
     }
+
     onTabChange = (key) => {
         this.setState({
             type: key,
         });
     }
+
     changeAutoLogin = (e) => {
         this.setState({
             autoLogin: e.target.checked,
         });
     }
+
     render() {
         return (
             <div className='container'>
                 <div className='login-page' >
-                    <div className='logo-container' />
                     <Login
                         defaultActiveKey={this.state.type}
                         onTabChange={this.onTabChange}
@@ -52,12 +61,12 @@ class LoginPage extends React.Component {
                                 this.state.notice &&
                                 <Alert style={{ marginBottom: 24 }} message={this.state.notice} type="error" showIcon closable />
                             }
-                            <UserName name="username" />
-                            <Password name="password" />
+                            <UserName name="username" rules={[{ required: true, message: '请输入账号或邮箱' }]} placeholder='用户名 / 邮箱账号' />
+                            <Password name="password" rules={[{ required: true, message: '请输入密码' }]} placeholder='密码' />
                         </Tab>
                         <Tab key="tab2" tab="手机">
-                            <Mobile name="mobile" />
-                            <Captcha onGetCaptcha={() => console.log('Get captcha!')} name="captcha" />
+                            <Mobile name="mobile" placeholder='手机号码' />
+                            <Captcha placeholder='验证码' onGetCaptcha={() => console.log('Get captcha!')} name="captcha" />
                         </Tab>
                         <div>
                             <Checkbox checked={this.state.autoLogin} onChange={this.changeAutoLogin}>记住我</Checkbox>
@@ -65,11 +74,11 @@ class LoginPage extends React.Component {
                         </div>
                         <Submit>登录</Submit>
                         <div>
-                            其他登录方式
-                         <span className="icon icon-alipay" />
-                            <span className="icon icon-taobao" />
-                            <span className="icon icon-weibo" />
-                            <a style={{ float: 'right' }} href="">注册</a>
+                            {/* 其他登录方式
+                            <Icon type="weibo" style={{ fontSize: 20 }} />
+                            <Icon type="wechat" style={{ fontSize: 20 }} />
+                            <Icon type="google" style={{ fontSize: 20 }} /> */}
+                            <a style={{ float: 'right' }} href="/register">注册</a>
                         </div>
                     </Login>
                 </div>
---
+++ src/routes/Register/Register.js
@@ -0,0 +1,193 @@
+import React from 'react';
+import Footer from '../../components/GlobalFooter/GlobalFooter';
+import { Form, Input, Col, Button, Radio, Alert } from 'antd';
+import { Register, validateCaptcha } from '../../service/User.service';
+import './index.scss';
+const FormItem = Form.Item;
+const RadioGroup = Radio.Group;
+const InputGroup = Input.Group;
+
+class RegisterPage extends React.Component {
+
+    state = {
+        captchaSuccess: false,   //用户是否输入正确的验证码,
+        formIsValidate: true
+    }
+
+    compareToFirstPassword = (rule, value, callback) => {
+        const form = this.props.form;
+        if (value && value !== form.getFieldValue('password')) {
+            callback('两次输入密码不相同');
+        } else {
+            callback();
+        }
+    }
+
+    onSubmit = () => {
+        const { captchaSuccess } = this.state;
+        const formData = this.props.form.getFieldsValue();
+        const { email, password, confirmPwd } = formData;
+        const role = [];
+        role.push({ name: formData.role })
+        formData.role = role;
+        formData.username = formData.email;
+        this.setState({ formIsValidate: true }, () => {
+            if (email && password && confirmPwd && (password === confirmPwd) && captchaSuccess) {
+                Register(formData).then(res => {
+                    if (res && res.status === 0) {
+                        window.location.href = '/classroom'
+                    }
+                });
+            } else {
+                this.setState({ formIsValidate: false })
+            }
+        })
+    }
+
+    /**
+     * 点击切换验证码
+     */
+    switchCaptcha = (e) => {
+        e.stopPropagation();
+        const img = document.createElement('img');
+        const that = e.currentTarget;
+        img.onload = function () {
+            that.innerHTML = '';
+            that.appendChild(img);
+        }
+        img.src = '/login/defaultKaptcha';
+    }
+
+    /**
+     * 验证码异步校验
+     */
+    validateCaptcha = (rule, value, callback) => {
+        if (value) {
+            setTimeout(() => {
+                validateCaptcha(value).then(res => {
+                    if (res) {
+                        this.setState({ captchaSuccess: true })
+                        callback();
+                    } else {
+                        this.setState({ captchaSuccess: false })
+                        callback('验证码错误');
+                    }
+                })
+            }, 500);
+        } else {
+            callback();
+        }
+    }
+
+    render() {
+        const { getFieldDecorator } = this.props.form;
+        return (
+            <div className='container'>
+                <div className='login-page' >
+                    {
+                        !this.state.formIsValidate ? (
+                            <Alert
+                                message="错误"
+                                description="请检查表单字段是否填写完整或正确"
+                                type="error"
+                                closable
+                            />
+                        ) : null
+                    }
+                    <Form layout='horizontal' style={{ marginTop: 30 }} >
+                        <FormItem label='注册'>
+                            {
+                                getFieldDecorator('email', {
+                                    rules: [{
+                                        required: true,
+                                        message: '请输入邮箱'
+                                    }, {
+                                        type: 'email',
+                                        message: '邮箱格式不合法'
+                                    }]
+                                })(
+                                    <Input placeholder='邮箱' type='email' size='large' />
+                                )
+                            }
+                        </FormItem>
+                        <FormItem>
+                            {
+                                getFieldDecorator('password', {
+                                    rules: [
+                                        { required: true, message: '请输入密码' }, {
+                                            min: 6,
+                                            message: '密码至少6位'
+                                        }, {
+                                            max: 16,
+                                            message: '密码最大长度只能为16位'
+                                        }]
+                                })(
+                                    <Input placeholder='6-16位密码，区分大小写' type='password' size='large' />
+                                )
+                            }
+                        </FormItem>
+                        <FormItem>
+                            {
+                                getFieldDecorator('confirmPwd', {
+                                    rules: [
+                                        { required: true, message: '请确认密码' },
+                                        {
+                                            validator: this.compareToFirstPassword
+                                        }
+                                    ]
+                                })(
+                                    <Input placeholder='确认密码' type='password' size='large' />
+                                )
+                            }
+                        </FormItem>
+                        <FormItem>
+                            <InputGroup>
+                                <Col span={17} >
+                                    {
+                                        getFieldDecorator('vrifyCode', {
+                                            rules: [{
+                                                required: true,
+                                                message: '请输入验证码'
+                                            }, {
+                                                validator: this.validateCaptcha
+                                            }]
+                                        })(
+                                            <Input placeholder='请输入验证码' size='large' />
+                                        )
+                                    }
+                                </Col>
+                                <Col span={7} >
+                                    <div onClick={this.switchCaptcha} style={{ cursor: 'pointer' }} title='点击更换验证码' ><img src='/login/defaultKaptcha' alt='captcha' /> </div>
+                                </Col>
+                            </InputGroup>
+                        </FormItem>
+                        <FormItem label='请选择登录角色' >
+                            {
+                                getFieldDecorator('role', {
+                                    rules: [{ required: true }],
+                                    initialValue: 'STUDENT'
+                                })(
+                                    <RadioGroup onChange={this.onChange} size='large'>
+                                        <Radio value={'STUDENT'}>学生</Radio>
+                                        <Radio value={'TEACHER'}>老师</Radio>
+                                    </RadioGroup>
+                                )
+                            }
+                        </FormItem>
+                        <FormItem>
+                            <Col span={12}>
+                                <Button style={{ width: '100%' }} size='large' type='primary' onClick={this.onSubmit} >注册</Button>
+                            </Col>
+                            <Col span={12}>
+                                <a style={{ float: 'right' }} href='/signin' >使用已有账号登录</a>
+                            </Col>
+                        </FormItem>
+                    </Form>
+                </div>
+                <Footer />
+            </div >
+        );
+    }
+}
+
+export default RegisterPage = Form.create()(RegisterPage);
